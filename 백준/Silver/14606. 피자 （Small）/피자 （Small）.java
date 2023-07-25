7d30ff0206d82283471917128304a8150d67b599
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2023-07-25 23:34:41 +0900

    [Silver V] Title: 피자 (Small), Time: 204 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
-public class Main {
-    public static int sum=0;
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        dfs(n);
-        System.out.println(sum);
-    }
+import static java.lang.Math.max;
 
-    private static void dfs(int num){
-        if(num==1)
-            return;
-        if(num%2==0){
-            sum+=num*num/4;
-            dfs(num/2);
-            dfs(num/2);
-        }else{
-            sum+=((num-1)*(num+1)/4);
-            dfs((num-1)/2);
-            dfs((num+1)/2);
+public class Main {
+    public static int[] dp;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp=new int[11];
+        dp[1]=0;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<i;j++){
+                dp[i]=max(dp[i],dp[j]+dp[i-j]+j*(i-j));
+            }
         }
+        System.out.println(dp[n]);
     }
 }
