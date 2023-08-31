b1d90a08109a47e976660c929411214bfb652946
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2023-08-31 23:47:59 +0900

    [Gold IV] Title: 플로이드, Time: 436 ms, Memory: 42948 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static boolean[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n + 1][n + 1];
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(arr[i], 100000000);
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = Math.min(c, arr[a][b]);
+
+        }
+        for (int i = 1; i <= n; i++) {
+            arr[i][i] = 0;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) arr[i][j] = arr[i][k] + arr[k][j];
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (arr[i][j] == 100000000) sb.append(0).append(' ');
+                else sb.append(arr[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
