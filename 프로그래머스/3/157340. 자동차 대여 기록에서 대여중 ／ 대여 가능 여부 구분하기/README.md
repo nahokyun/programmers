eafb4a835b2d051696b11e84a918b8515ff33bf0
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2025-06-05 10:55:51 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/README.md
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기 - 157340 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157340) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157340?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 11월 22일 11:09:30
+2025년 06월 05일 10:55:48
 
 ### 문제 설명
 
--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,11 +1,10 @@
 -- 코드를 입력하세요
-SELECT car_id, case when car_id in(
-select car_id
-    from car_rental_company_rental_history
-    where start_date<='2022-10-16' and end_date>='2022-10-16'
-)then '대여중' 
-else '대여 가능'
-end as availability
-from car_rental_company_rental_history as cr
+SELECT car_id, case when car_id in (select car_id
+                              from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                              where start_date<=to_date('2022-10-16','YYYY-MM-DD') and end_date>=to_date('2022-10-16','YYYY-MM-DD')) 
+    then '대여중'
+    else '대여 가능'
+    end availability
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY cr
 group by car_id
 order by car_id desc
