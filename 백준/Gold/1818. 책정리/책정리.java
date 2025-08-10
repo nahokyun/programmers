a14834555ffcd7ce24a7e8885848f4b425989923
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2025-08-10 14:36:06 +0900

    [Gold II] Title: 책정리, Time: 332 ms, Memory: 33808 KB -BaekjoonHub

---
+++ 백준/Gold/1818. 책정리/책정리.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		int[] arr=new int[n];
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++){
+			arr[i]=Integer.parseInt(st.nextToken());
+		}//입력 종료
+        
+        int[] longestArr=new int[n+1];
+        longestArr[0]=arr[0];
+        
+        int idx=0;
+        for(int i=1;i<n;i++){
+            int cur=arr[i];
+            
+            if(cur>longestArr[idx]){
+                longestArr[++idx]=cur;
+            }else{
+                int left=-1;
+                int right=idx;
+                while(left+1<right){
+                    int mid=(left+right)>>1;
+                    if(longestArr[mid]>cur){
+                        right=mid;
+                    }else{
+                        left=mid;
+                    }
+                }
+                longestArr[right]=cur;
+            }//이분탐색 종료
+        }//for문 종료
+        
+                
+        System.out.println(n-idx-1);
+        
+        
+    }
+}
