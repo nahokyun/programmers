c825572eeb14068cadf5e2972d4729c6831775c9
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2025-12-04 11:56:04 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 124 ms, Memory: 15120 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+        int n=Integer.parseInt(br.readLine());
+        Set<String> set=new HashSet<>();
+        set.add("ChongChong");
+        
+        for(int i=0;i<n;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            String first=st.nextToken();
+            String second=st.nextToken();
+
+            if(set.contains(first)){
+                set.add(second);
+            }
+            if(set.contains(second)){
+                set.add(first);
+            }
+        }
+        
+        System.out.println(set.size());
+    }
+}
