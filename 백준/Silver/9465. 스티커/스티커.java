934267ba1466baac4d9f884a3ea6d2ebda12cead
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2025-12-10 15:21:22 +0900

    [Silver I] Title: 스티커, Time: 648 ms, Memory: 111552 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -12,24 +12,28 @@ public class Main {
         for(int x=0;x<T;x++){
             int n=Integer.parseInt(br.readLine());
             StringTokenizer st;
-            int[][] arr=new int[4][n+1];
-            int[][] dp=new int[4][n+1];
-            for(int j=1;j<=2;j++){
+            int[][] arr=new int[2][n+1];
+            int[][] dp=new int[2][n+1];
+            for(int j=0;j<2;j++){
                 st=new StringTokenizer(br.readLine());
                 for(int i=1;i<=n;i++){
                     arr[j][i]=Integer.parseInt(st.nextToken());
                 }
             }//입력종료
             
+            dp[0][1]=arr[0][1];
             dp[1][1]=arr[1][1];
-            dp[2][1]=arr[2][1];
             for(int j=2;j<=n;j++){
-                for(int i=1;i<=2;i++){
-                    dp[i][j]=Math.max(Math.max(dp[i-1][j-1],dp[i-1][j-2]),Math.max(dp[i+1][j-1],dp[i+1][j-2]))+arr[i][j];
+                for(int i=0;i<2;i++){
+                    if(i==0){
+                        dp[i][j]=Math.max(dp[1][j-1],dp[1][j-2])+arr[i][j];
+                    }else{
+                        dp[i][j]=Math.max(dp[0][j-1],dp[0][j-2])+arr[i][j];
+                    }
                 }
             }
             //탐색 종료
-            sb.append(Math.max(dp[1][n],dp[2][n])).append('\n');
+            sb.append(Math.max(dp[0][n],dp[1][n])).append('\n');
         }
         
         
