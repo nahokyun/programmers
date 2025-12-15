490905eef3ec1ab721d5e491dd012807aee08c2c
Na hokyun <100259486+nahokyun@users.noreply.github.com>
2025-12-15 11:25:59 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 136 ms, Memory: 22176 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        int[] test=new int[t];
+        int max=0;
+        StringBuilder sb=new StringBuilder();
+        long[] arr=new long[1000001];
+        arr[1]=1;
+        arr[2]=2;
+        arr[3]=4;
+        
+        for(int i=0;i<t;i++){
+            test[i]=Integer.parseInt(br.readLine());
+            max=Math.max(test[i],max);
+        }
+        
+        for(int i=4;i<=max;i++){
+            arr[i]=(arr[i-3]+arr[i-2]+arr[i-1])%1_000_000_009;
+        }
+        
+        for(int i=0;i<t;i++){
+            sb.append(arr[test[i]]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
